423837167c499f7a96e2ab96549004cb56569283
muhammetfurkankorkmaz <225040043@stu.istinye.edu.tr>
2025-04-26 18:08:22 +0300

    Add project files.

---
+++ DGD-Assigment1- Final/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DGD_Assigment1__Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
