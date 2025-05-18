7f4483fcaf356e9ee2e39f3be59dce25b882a916
muhammetfurkankorkmaz <225040043@stu.istinye.edu.tr>
2025-05-18 14:40:23 +0300

    Start of the game and main menu updates

--- DGD-Assigment1- Final/Program.cs
+++ DGD-Assigment1- Final/Program.cs
@@ -2,16 +2,67 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DGD_Assigment1__Final
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+        private bool _isRunning;
 
-            //This is for testing haha
+        static async Task Main(string[] args)
+        {
+            Program program = new Program();
+            await program.GameLoop();
         }
-    }
-}
+        public async Task GameLoop()
+        {
+            Initialize();
+
+            _isRunning = true;
+            while (_isRunning)
+            {
+                // Display menu and get player input
+                string userChoice = GetUserInput();
+
+                // Process the player's choice
+                await ProcessUserChoice(userChoice);
+            }
+
+            Console.WriteLine("Thanks for playing!");
+        }
+        private void Initialize()
+        {
+            StartMenu startMenu = new StartMenu();
+            startMenu.GameStartMenu();
+            Console.WriteLine("Tamagotchi is starting now!!!");
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            Thread.Sleep(1000);
+            Console.Clear();           
+
+        }
+
+        private string GetUserInput()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("WELCOME TO TAMAGOTCHI");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("1- Adopt a pet");
+            Console.WriteLine("2- Look your pets");
+            Console.WriteLine("3- Spend time with your pets");
+            Console.WriteLine("4- Exit the Tamagotchi");
+
+            return Console.ReadLine();
+        }
+
+        private async Task ProcessUserChoice(string choice)
+        {
+            // Use this to process any choice user makes
+            // Set _isRunning = false to exit the game
+        }
+    }//CLASS
+}//NAMESPACE
---
+++ DGD-Assigment1- Final/StartMenu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DGD_Assigment1__Final
+{
+    internal class StartMenu
+    {
+        public void GameStartMenu()
+        {
+            bool _isGameInitialized = false;
+            while (!_isGameInitialized)
+            {
+                Console.WriteLine("1- Start the Tamagotchi");
+                Console.WriteLine("2- Credits");
+                string userInput = Console.ReadLine();
+                if (userInput == "1")
+                {
+                    _isGameInitialized = true;
+                }
+                else if (userInput == "2")
+                {
+                    CreditsScreen();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("INVALID INPUT!!!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+        }
+        void CreditsScreen()
+        {
+            Console.WriteLine("M. Furkan Korkmaz 225040043");
+            Console.WriteLine("Press any button to proceed.");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }//Class
+}//NameSpace
